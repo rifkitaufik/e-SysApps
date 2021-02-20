c311621f1fdfdc2b0efb41cec42c174c5d4811d1
rifkitaufik <rifkitaufik01@gmail.com>
2021-02-21 01:30:53 +0700

    update tulisan di demo

--- app/Views/aplikasi/demo.php
+++ app/Views/aplikasi/demo.php
@@ -28,7 +28,7 @@
                                     </ul>
                                 </div>
                                 <h2>
-                                    <a href="#">Overcame breeding or my concerns removings of management. </a>
+                                    <a href="#">Demo Aplikasi e-SysApps </a>
                                 </h2>
                                 <p>
                                     Give lady of they such they sure it. Me contained explained my education. Vulgar as hearts by garret. Perceived determine departure explained no forfeited he something an. Contrasted dissimilar get joy you instrument out reasonably. Again keeps at no meant stuff. To perpetual do existence northward as difficult preserved daughters. Continued at up to zealously necessary breakfast. Surrounded sir motionless she end literature. Gay direction neglected but supported yet her.
