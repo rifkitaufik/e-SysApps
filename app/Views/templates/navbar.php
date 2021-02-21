bb0f1a6e279f0d1aa11792dcbc29fa2e1b57bd73
rifkitaufik <rifkitaufik01@gmail.com>
2021-02-21 10:03:23 +0700

    perubahan pada struktur tema, nav pindah keluar

---
+++ app/Views/templates/navbar.php
@@ -0,0 +1,51 @@
+ <!-- Header 
+    ============================================= -->
+ <header id="home">
+
+     <!-- Start Navigation -->
+     <nav class="navbar navbar-default navbar-sticky bootsnav on no-full">
+
+         <div class="container">
+
+             <!-- Start Atribute Navigation -->
+             <div class="attr-nav">
+                 <ul>
+                     <li class="button"><a href="https://wa.me/+6281343042761" target="_blank">Hubungi</a></li>
+                 </ul>
+             </div>
+             <!-- End Atribute Navigation -->
+
+             <!-- Start Header Navigation -->
+             <div class="navbar-header">
+                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                     <i class="fa fa-bars"></i>
+                 </button>
+                 <a class="navbar-brand" href="http://localhost:8080/">
+                     <img src="/assets/img/logo.png" class="logo" alt="Logo">
+                 </a>
+             </div>
+             <!-- End Header Navigation -->
+
+             <!-- Collect the nav links, forms, and other content for toggling -->
+             <div class="collapse navbar-collapse" id="navbar-menu">
+                 <ul class="nav navbar-nav navbar-center" data-in="#" data-out="#">
+                     <li>
+                         <a href="#" class="smooth-menu active">Home</a>
+                     </li>
+                     <li>
+                         <a class="smooth-menu" href="#fitur">Fitur</a>
+                     </li>
+                     <li>
+                         <a class="smooth-menu" href="#aplikasi">Aplikasi</a>
+                     </li>
+                     <li>
+                         <a class="smooth-menu" href="#harga">Harga</a>
+                     </li>
+                 </ul>
+             </div><!-- /.navbar-collapse -->
+         </div>
+     </nav>
+     <!-- End Navigation -->
+
+ </header>
+ <!-- End Header -->
